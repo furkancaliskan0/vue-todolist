38d5d2e77387d11fffea07979f21cfcfee3017a4
furkanGit <furkancaliskan9801@gmail.com>
2022-11-29 23:12:10 +0100

    Completed

--- js/main.js
+++ js/main.js
@@ -2,3 +2,57 @@
 ma con una struttura di array di oggetti e non un array di semplici stringhe.*/
 
 
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+    logo:  './img/logo copia.png',  
+    errore: false,
+    newTask:'',
+      tasks: [
+          
+        {
+            text: "Andare a fare la spesa",
+            done: true
+         },
+        {
+            text: "Fare la spesa",
+              done: true 
+        },
+        {
+            text: "Controllare le Email",
+            done: false
+        },
+        {
+            text: "Mettere la benzina nella macchina",
+            done: false
+        },
+        {
+            text: "Fare i compiti", 
+            done: false 
+        },
+        {
+            text: "Portare fuori la spazzatura",
+            done: false
+        },
+        {
+            text: "Passare al bar",
+            done: true
+        }
+      ],
+    }
+  },
+  methods : {
+    addTask : function(){
+        if (this.newTask !== '') {
+            this.tasks.unshift({text: this.newTask, done: false})
+        }
+        this.newTask = '';
+    },
+
+  
+  }
+}).mount("#list")
+
+
